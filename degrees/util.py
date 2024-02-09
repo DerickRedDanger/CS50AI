13a9b25da39bc44d5a966fa4b67e93770f943c88
DerickRedDanger <101509965+DerickRedDanger@users.noreply.github.com>
2024-02-08 22:43:35 -0300

    Initial commit

---
+++ degrees/util.py
@@ -0,0 +1,43 @@
+class Node():
+    def __init__(self, state, parent, parent_movie, action,level):
+        self.state = state
+        self.parent = parent
+        self.parent_movie= parent_movie
+        #
+        # self.parent_movie= parent_movie change this in the degree
+        #
+        self.action = action
+        self.level=level
+
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
