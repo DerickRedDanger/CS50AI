4711fc31a858ea1afd24553d240ade7adb25cb42
DerickRedDanger <101509965+DerickRedDanger@users.noreply.github.com>
2024-04-18 18:07:27 -0300

    Project 3 - optimization - crossword - update
    Created a queue_2s function that uses two stacks to emulate a queue. Initially felt useful considering the size of some word.txt files,  but on a second look, it will only be used to store variables (the spaces for words in structures.txt), and there aren't many of these, this might've been unnecessary, but still served as a learning experience in creating classes.

--- Projects_3-Optimization/crossword/generate.py
+++ Projects_3-Optimization/crossword/generate.py
@@ -148,6 +148,19 @@ class CrosswordCreator():
         Return True if arc consistency is enforced and no domains are empty;
         return False if one or more domains end up empty.
         """
+        
+
+        if arcs == None:
+            queue = (var for var in self.crossword.variables)
+            queue = queue_2s(queue)
+        else:
+            queue = queue_2s(arcs)
+
+        # while the queue has more then 0 elements
+        while queue.check():
+            queue.enqueue(x)
+            value = queue.dequeue()
+
         raise NotImplementedError
 
     def assignment_complete(self, assignment):
@@ -194,6 +207,48 @@ class CrosswordCreator():
         """
         raise NotImplementedError
 
+class queue_2s():
+    def __init__(self, queue=None):
+        """
+        A queue made by using two stacks.
+
+        A queue can be computationally costly as the list increase in size. This happens because
+        poping a element that isn't the last from a list, forces python to reorganize the whole list,
+        moving the elements after that element foward.
+        
+        That's why I am using two stacks instead. we will keep appending elements to the first stack, keeping them in order.
+        But when we need to dequeue elements, if S2 is already empty, we will pop elements from the first list and append them in the second list,
+        So that the first element in the first list become the last in the second one, allowing us to simply pop them in order.
+        If s2 is not empty, then we can just continue to pop it's last element, only adding elements from S1 when it's empty again.
+        """
+        if not queue:
+            self.s1 = []
+        else:
+            self.s1=queue
+
+        self.s2 = []
+
+    def enqueue(self,item):
+
+        self.s1.append(item)
+
+    def dequeue(self):
+        if not self.s1 and not self.s2:
+            raise QueueEmptyError("Queue is empty")
+
+        elif not self.s2 and self.s1:
+            for _ in range(len(self.s1)):
+                self.s2.append(self.s1.pop())
+    
+        return self.s2.pop()
+    
+    
+
+    def check(self):
+        return len(self.s1) + len(self.s2)
+                
+class QueueEmptyError(Exception):
+        pass
 
 def main():
 
